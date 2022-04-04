df583454184abb4a6fbcc09387cb739c723aa352
Arturs Volkausks <artja201@yandex.com>
2022-04-04 11:39:07 +0300

    Installed React and connected it with backend. To build frontend run mvn clean install

--- src/main/java/com/bootcamp/wordle/SimpleController.java
+++ src/main/java/com/bootcamp/wordle/SimpleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SimpleController {
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String welcome()
     {
         return "<html><body>"
