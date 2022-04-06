1bfcdd58299d171da1251ed756f6cacc58f60516
Andrejs Vasiljevs <OpikolG@protonmail.com~>
2022-04-06 14:42:04 +0300

    Added Scheduler for deleting games

--- src/main/java/com/bootcamp/wordle/WordleApplication.java
+++ src/main/java/com/bootcamp/wordle/WordleApplication.java
@@ -2,11 +2,13 @@ package com.bootcamp.wordle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 @SpringBootApplication
+@EnableScheduling
 public class WordleApplication {
 	public static Connection db = null;
 	public static void main(String[] args) {
