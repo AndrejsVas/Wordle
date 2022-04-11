402b781e2597e322dae9a3a512abfd1926996278
Deniel <briuversd@gmail.com>
2022-04-11 11:50:27 +0300

    Added User screen (input)

--- frontend/src/components/UserStartPop.js
+++ frontend/src/components/UserStartPop.js
@@ -6,8 +6,19 @@ class UserStartPop extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          countStart: true
+          countStart: true,
+          value: ''
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
       }
       userChange = () => {
         this.setState({ countStart: false });
@@ -16,7 +27,13 @@ class UserStartPop extends Component {
     return (
       <div className='userStartPop'>           
             <UserStart trigger ={this.state.countStart} setTrigger ={this.userChange}>
-                    <h1>Enter your Username:</h1>                   
+                    <h1>Enter your Username:</h1> 
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                    <input type="submit" value="Submit" />
+                   </form>                  
             </UserStart>        
       </div>
       
