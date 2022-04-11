96a711d5a73e03938e0066dce021e74c843dab13
Arturs Volkausks <artja201@yandex.com>
2022-04-11 15:10:32 +0300

    Added random userName and changed createGameSession to post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+
+import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator'
+
 import RulesPop from './components/RulesPop';
+import Game from './components/Game';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import Game from './components/Game';
 
 class App extends Component {
 
+    state = {
+        userName: uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] })
+    }
+
     render() {
+        console.log(this.state.userName);
         return (
             <div className="App">
                 <header className="App-header">
-                    <Game />
+                    <Game userName={this.state.userName} />
                  <RulesPop />
                 </header>                             
             </div>
