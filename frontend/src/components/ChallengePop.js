2cd850e0f0a433b68ae98443b22b1523af82710c
Deniel <briuversd@gmail.com>
2022-04-12 17:03:10 +0300

    Added non-functional Challenge link Icon with Popup

---
+++ frontend/src/components/ChallengePop.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Challenge from './Challenge'
+import './Challenge.css'
+import challengeIcon from '../images/challenge-icon.jpg';
+
+
+class ChallengePop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: false
+        };
+      }
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        this.props.setUserName(this.state.value);
+        event.preventDefault();
+      }
+      popupChange = () => {
+        this.setState({ count: false });
+    }
+  render() {
+    return (
+      <div className='challengepop'>
+          <img id='challenge-img' src={challengeIcon} alt="Challenge Icon" onClick={() => this.setState({ count: true })}></img>
+        <Challenge trigger ={this.state.count} setTrigger ={this.popupChange}>
+            <h1>Set your word:</h1>
+            <input id="userName-input" type="text" value={this.state.value} onChange={this.handleChange}  />
+            <input id="userName-submit" type="submit" value="Submit" onClick={async () =>{await this.props.setUserName(this.state.value); this.userChange();this.props.gameStartState();}} />
+
+        </Challenge>
+          </div>
+    )
+  }
+}
+export default ChallengePop
