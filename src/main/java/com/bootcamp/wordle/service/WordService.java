73ac54fd7be47ffecf4f109b908fdcb812126fed
Andrejs Vasiljevs <OpikolG@protonmail.com~>
2022-04-12 07:47:10 +0300

    Checks both word and extended word schemas. Fixed indentation

--- src/main/java/com/bootcamp/wordle/service/WordService.java
+++ src/main/java/com/bootcamp/wordle/service/WordService.java
@@ -1,4 +1,5 @@
 package com.bootcamp.wordle.service;
+
 import com.bootcamp.wordle.model.Word;
 import com.bootcamp.wordle.repository.ExtendedWordRepository;
 import com.bootcamp.wordle.repository.WordRepository;
@@ -19,6 +20,7 @@ public class WordService {
     private WordRepository wordRepository;
     @Autowired
     private ExtendedWordRepository extendedWordRepository;
+
     public List<Word> getAllWords() {
         List<Word> wordList = null;
         try {
@@ -27,52 +29,55 @@ public class WordService {
 
         } catch (NullPointerException e) {
             System.out.println("List not populated.");
-        } catch (RangeException e){
+        } catch (RangeException e) {
             throw e;
         }
         return wordList;
     }
 
-    public boolean getWordByName(String tryWord){
-        return extendedWordRepository.existsByWord(tryWord);
+    //TODO: Maybe better to do joins?
+    public boolean getWordByName(String tryWord) {
+        if (extendedWordRepository.existsByWord(tryWord) || wordRepository.existsByWord(tryWord))
+            return true;
+        else
+            return false;
     }
 
-    public int[] compareWords(String userSubmittedWord, String gameSessionWord){
+    public int[] compareWords(String userSubmittedWord, String gameSessionWord) {
         int[] letterPlacement = new int[5];
-        try{
-        char[] userSubmittedCharArray  = userSubmittedWord.toLowerCase().toCharArray();
-        char[] gameSessionCharArray = gameSessionWord.toLowerCase().toCharArray();
-        //Hardcoded Size for now
+        try {
+            char[] userSubmittedCharArray = userSubmittedWord.toLowerCase().toCharArray();
+            char[] gameSessionCharArray = gameSessionWord.toLowerCase().toCharArray();
+            //Hardcoded Size for now
 
-        for(int i = 0; i < userSubmittedCharArray.length; i++) {
-            if (gameSessionCharArray[i] == userSubmittedCharArray[i]) {
-                letterPlacement[i] = 3;
-                gameSessionCharArray[i] = ' ';
-            }
-        }
-        for(int i = 0; i < userSubmittedCharArray.length; i++) {
-            if (gameSessionCharArray[i] != ' ') {
-                for (int j = 0; j < gameSessionCharArray.length; j++) {
-                    if (userSubmittedCharArray[i] == gameSessionCharArray[j]) {
-                        letterPlacement[i] = 2;
-                    }
+            for (int i = 0; i < userSubmittedCharArray.length; i++) {
+                if (gameSessionCharArray[i] == userSubmittedCharArray[i]) {
+                    letterPlacement[i] = 3;
+                    gameSessionCharArray[i] = ' ';
                 }
-                if (letterPlacement[i] != 2 && letterPlacement[i] != 3)
-                    letterPlacement[i] = 1;
             }
-        }
-        } catch(ArrayIndexOutOfBoundsException e){
+            for (int i = 0; i < userSubmittedCharArray.length; i++) {
+                if (gameSessionCharArray[i] != ' ') {
+                    for (int j = 0; j < gameSessionCharArray.length; j++) {
+                        if (userSubmittedCharArray[i] == gameSessionCharArray[j]) {
+                            letterPlacement[i] = 2;
+                        }
+                    }
+                    if (letterPlacement[i] != 2 && letterPlacement[i] != 3)
+                        letterPlacement[i] = 1;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Wrong input size.");
         }
         return letterPlacement;
 
     }
 
-    public String findRandomWord(){
+    public String findRandomWord() {
         Word word = wordRepository.findRandomWord();
         return word.getWord();
     }
-
 
 
 }
