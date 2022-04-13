c3266eee1f32ce7c293c1f592c7b61b5899ab687
Andrejs Vasiljevs <OpikolG@protonmail.com~>
2022-04-14 02:29:27 +0300

    Updated endpoint documentation

--- src/main/java/com/bootcamp/wordle/controller/WordController.java
+++ src/main/java/com/bootcamp/wordle/controller/WordController.java
@@ -2,6 +2,7 @@ package com.bootcamp.wordle.controller;
 
 
 import com.bootcamp.wordle.service.WordService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +13,7 @@ public class WordController {
     @Autowired
     private WordService wordService;
 
+    @Operation(summary = "Get a random word")
     @GetMapping(value = "/api/word/getRandomWord")
     public String createGameSession( ){
         return wordService.findRandomWord();
